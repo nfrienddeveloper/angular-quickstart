88acf963e753d879f4299a02a5b2c06a023ace13
Nathan D Friend <nfriend.developer@gmail.com>
2022-03-17 15:48:01 -0400

    Test Http Client Requests to DDC API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'angular-quickstart';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(){
+     
+  }
+
+  onCreateUser(postData: { email: string, name: string){
+    this.http.post('https://ddc-v2-staging-api.herokuapp.com/orders', {
+      email: 'nfriend.developer@gmail.com',
+      name: 'Nathan Friend'
+    }).subscribe(data =>{
+      console.log(data)
+    });
+  }
+
+  onCreateOrder(){
+
+  }
+
+
 }
